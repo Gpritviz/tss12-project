818d48db870fbf3431edeed32cdc3c524839bef6
Gpritviz <riya1109indorey@gmail.com>
2023-10-18 15:00:05 +0530

    live momgodb url insert

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-require("mongoose").connect("mongodb://0.0.0.0:27017/new_blog").then(()=>{
+require("mongoose").connect("mongodb+srv://riya1109indorey:o6HmpBakqk1KY2ii@cluster0.ldvvgwg.mongodb.net/?retryWrites=true&w=majority").then(()=>{
   console.log("DATA BASE CONNECTED");
 }).catch((err)=>{
        console.log("NOT CONNECTED--------",err); 
